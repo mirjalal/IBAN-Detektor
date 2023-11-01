7978124fecc335ded71809eaa5139f70e6994db7
Mirjalal <mirjalal.talishinski@gmail.com>
2023-11-01 17:37:07 +0300

    use API33 method call

--- app/src/main/java/com/minkiapps/scanner/util/View.kt
+++ app/src/main/java/com/minkiapps/scanner/util/View.kt
@@ -4,12 +4,16 @@ import android.app.Activity
 import android.content.Context
 import android.content.res.Configuration
 import android.content.res.TypedArray
+import android.os.Build
 import java.io.Serializable
 
 inline fun <reified T : Enum<T>> TypedArray.getEnum(index: Int, default: T) =
     getInt(index, -1).let { if (it >= 0) enumValues<T>()[it] else default }
 
-fun <T : Serializable> Activity.extraSerializableOrThrow(key : String) = lazy {
+inline fun <reified T : Serializable> Activity.extraSerializableOrThrow(key : String) = lazy {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+        intent?.getSerializableExtra(key, T::class.java) ?: throw RuntimeException("no extra found for key $key in intent")
+    else
         intent?.getSerializableExtra(key) as T? ?: throw RuntimeException("no extra found for key $key in intent")
 }
 
